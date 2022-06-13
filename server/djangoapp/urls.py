40f857edef11a12351f9a3c592114c40bc796098
juliommen <juliommen@gmail.com>
2022-06-13 19:25:34 +0000

    update

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,9 +5,21 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # route is a string contains a URL pattern
+    # route is a string that contains a URL pattern
     # view refers to the view function
     # name the URL
+    path(route='', view=views.get_dealerships, name='index'),
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+
+
+
+    #path('<int:course_id>/enroll/', views.enroll, name='enroll'),
+    #path('<int:course_id>/submit/', views.submit, name='submit'),
+
 
     # path for about view
 
@@ -19,7 +31,7 @@ urlpatterns = [
 
     # path for logout
 
-    path(route='', view=views.get_dealerships, name='index'),
+    
 
     # path for dealer reviews view
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,16 +14,15 @@ import json
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
+def about(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/about.html', context)
 
-
-# Create an `about` view to render a static about page
-# def about(request):
-# ...
-
-
-# Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
@@ -52,3 +51,113 @@ def get_dealerships(request):
 # def add_review(request, dealer_id):
 # ...
 
+def registration_request(request):
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    elif request.method == 'POST':
+        # Check if user exists
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.error("New user")
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            login(request, user)
+            return redirect("djangoapp:index")
+        else:
+            context['message'] = "User already exists."
+            return render(request, 'djangoapp/registration.html', context)
+
+
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            context['message'] = "Invalid username or password."
+            return render(request, 'djangoapp/user_login.html', context)
+    else:
+        return render(request, 'djangoapp/user_login.html', context)
+
+
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
+
+
+'''
+def check_if_enrolled(user, course):
+    is_enrolled = False
+    if user.id is not None:
+        # Check if user enrolled
+        num_results = Enrollment.objects.filter(user=user, course=course).count()
+        if num_results > 0:
+            is_enrolled = True
+    return is_enrolled
+
+
+def enroll(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    user = request.user
+
+    is_enrolled = check_if_enrolled(user, course)
+    if not is_enrolled and user.is_authenticated:
+        # Create an enrollment
+        Enrollment.objects.create(user=user, course=course, mode='honor')
+        course.total_enrollment += 1
+        course.save()
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id,)))
+
+def submit(request,course_id):
+    submitted_anwsers =  extract_answers(request)
+    username = request.user.username
+    user = User.objects.get(username=username)
+    enrollment = Enrollment.objects.get(user=user,course=course_id)
+    submission = Submission.objects.create(
+        enrollment=enrollment)
+    for choice in submitted_anwsers:
+        submission.choices.add(choice)
+    #print(submission.choices.filter(id=choice).)
+    context = show_exam_result(course_id, submission)
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
+
+
+def extract_answers(request):
+    submitted_anwsers = []
+    for key in request.POST:
+        if key.startswith('choice'):
+            value = request.POST[key]
+            choice_id = int(value)
+            submitted_anwsers.append(choice_id)
+    return submitted_anwsers
+
+
+def show_exam_result(course_id, submission):
+    course = Course.objects.get(id=course_id)
+    choices = submission.choices.all()
+    questions = course.question_set.all()
+    correct = 0
+    total = 0
+    for question in questions:
+        if (Question.is_get_score(question,choices)):
+            correct = correct + 1
+        total = total + 1
+    grade = correct * 100 / total
+    grade = int(grade)
+    context={'course':course, 'submission':submission,'grade':grade}
+    return context
+'''
+    
